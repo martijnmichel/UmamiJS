081f2558610e11a3f3a70b9af0f401ceae55ab51
martijnmichel <martijn.michel@gmail.com>
2024-06-22 15:29:28 +0200

    Initial Commit

---
+++ UmamiJS/UmamiJSApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmamiJSApp.swift
+//  UmamiJS
+//
+//  Created by Martijn van der Eijk on 22/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct UmamiJSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
